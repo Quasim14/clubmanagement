0b4eb9e89981639b660c26edc8622d6542edd17c
Quassim <nicomaxa@live.fr>
2019-10-08 08:13:47 +0200

    MemberService and MemberServiceImplementation created

--- src/main/java/com/clubmanagement/agoraz/AgorazApplication.java
+++ src/main/java/com/clubmanagement/agoraz/AgorazApplication.java
@@ -1,7 +1,9 @@
 package com.clubmanagement.agoraz;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AgorazApplication {
@@ -9,5 +11,10 @@ public class AgorazApplication {
     public static void main(String[] args) {
         SpringApplication.run(AgorazApplication.class, args);
     }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
 
 }
