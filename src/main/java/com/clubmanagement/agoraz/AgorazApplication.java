7a612c7b5a24a5c2ec95b19c6bf5893e6d3162fa
Quassim <nicomaxa@live.fr>
2019-10-08 07:35:39 +0200

    Initial commit Spring boot Maven initialized

---
+++ src/main/java/com/clubmanagement/agoraz/AgorazApplication.java
@@ -0,0 +1,13 @@
+package com.clubmanagement.agoraz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgorazApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgorazApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clubmanagement/agoraz/AgorazApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clubmanagement.agoraz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgorazApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
